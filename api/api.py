94f75b5cc94fd4ab55317fbf97afe61d8a8b6797
Phil Skentelbery <philskents@protonmail.com>
2020-09-10 15:54:59 -0500

    basic api achieved

--- api/api.py
+++ api/api.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+bookmarks = [
+    {
+        "title":"My Blog",
+        "url":"https://strangeadventures.in",
+        "tags":["blog"]
+    },
+    {
+        "title":"Splunk",
+        "url":"https://splunk.com",
+        "tags":["vendor"]
+    }
+]
+
+@app.route('/bookmarks')
+def get_bookmarks():
+    return jsonify(bookmarks)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
